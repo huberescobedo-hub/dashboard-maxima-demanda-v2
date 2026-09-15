1423017ad6851695ba1cfa242ec4d730fa1bf059
huberescobedo-hub <huber.escobedo@realplaza.com.pe>
2026-09-15 10:51:14 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/dashboard-maxima-demanda-v2/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
